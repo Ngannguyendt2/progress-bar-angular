253d89a83acb13a12c6078395a98a23aef6a6be0
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-10-25 10:20:07 +0700

    name card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameCardComponent } from './name-card/name-card.component';
+import { NameComponent } from './name/name.component';
+import { EmailComponent } from './email/email.component';
+import { PhoneComponent } from './phone/phone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameCardComponent,
+    NameComponent,
+    EmailComponent,
+    PhoneComponent
   ],
   imports: [
     BrowserModule,
