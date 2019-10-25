e9d8fd80d5a42dbb805d0f5e3a617e8a29e11efe
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-10-25 10:39:10 +0700

    progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { NameCardComponent } from './name-card/name-card.component';
 import { NameComponent } from './name/name.component';
 import { EmailComponent } from './email/email.component';
 import { PhoneComponent } from './phone/phone.component';
+import { ProgressBarComponent } from './progress/progress-bar/progress-bar.component';
+import { BackgroundComponent } from './progress/background/background.component';
+import { ColorComponent } from './progress/color/color.component';
+import { WidthComponent } from './progress/width/width.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { PhoneComponent } from './phone/phone.component';
     NameCardComponent,
     NameComponent,
     EmailComponent,
-    PhoneComponent
+    PhoneComponent,
+    ProgressBarComponent,
+    BackgroundComponent,
+    ColorComponent,
+    WidthComponent
   ],
   imports: [
     BrowserModule,
